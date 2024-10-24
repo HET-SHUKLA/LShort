a5b2034575f0dd0af3e25add7dd161c576f2ddbd
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-10-24 22:00:07 +0530

    add port in env

--- API/index.js
+++ API/index.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { mongo_url } from './config.js';
+import { mongo_url, port } from './config.js';
 import shortUrlRouter from './routes/shortUrl.js';
 import connectDB from './connection.js';
 import { errorHandler } from './middlewares/errorHandling.js';
 
-const PORT = 3000;
 const app = express();
 
 //Middlewares
@@ -16,7 +15,7 @@ connectDB(mongo_url).then( () => {
     console.log('Mongo DB Connected');
 
     //Starting server
-    app.listen(PORT, () => {
+    app.listen(port, () => {
         console.log('Server Started on port : ' + PORT);
     });
 
