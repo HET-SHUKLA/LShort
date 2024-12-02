30439dcbca84a8c912fc47b322996386deda4701
HET SHUKLA <SHUKLAHET2704@gmail.com>
2024-12-02 12:24:12 +0530

    Final Init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //server is only for dev
   server: {
     proxy: {
       '/api': 'https://lshort.up.railway.app/',
     }
   },
+  base: '/',
   plugins: [react()],
 })
